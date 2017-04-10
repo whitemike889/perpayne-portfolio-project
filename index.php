31852fb1595739c14301d0d73c5b1b9ca1c3a104
Maroua <marua_elba@hotmail.it>
2017-04-10 17:35:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Peter Characters and Illustrations</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,600&subset=latin,latin-ext&effect=wallpaper|shadow-multiple' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="peter/magnific-popup.css">
+        <!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+        <!-- Magnific Popup core JS file -->
+        <script src="magnific-popup/jquery.magnific-popup.js"></script>
+
+     
+        <!--External Javascript and css files-->
+        <script src="https://use.fontawesome.com/bc649fb0f5.js"></script>
+        <script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+        <script type="text/javascript" src="js/banner.js"></script>
+        <link href="css/jquery-banner-stylesheet.css" rel="stylesheet" type="text/css" />
+        <!--External Javascript and css files-->
+    </head>
+      <body>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class=" col-sm-12 page-header font-effect-shadow-multiple" ><p class="characters">Characters&nbsp</p>and <p class="illustrations">Illustrations</p>
+                <a href="index.php"><p class="peter-title">Peter Payne.com</p></a>
+              </div>
+            </div> 
+            <div class="row peter-menu">
+              <div class="header-menu">
+                <ul class="nav-home">
+                  <li><a class="peter-link" href="index.php"><p>Home</p></a>
+                  </li>
+                  <li><a class="peter-link" href="bio.php"><p>Bio</p></a>
+                  </li>
+                  <li><a class="peter-link" href="contacts.php"><p>Contacts</p></a> 
+                  </li>
+                  <li><a class="peter-link" href="illustrations-portfolio.php"><p>Portfolio</p></a>
+                </ul>
+              </div> 
+            </div>
+        <div class="peter-header font-effect-shadow-multiple ">    
+          <div class="row">  
+            <div class="col-sm-12">
+              <p class="hi-peter normal">Hi Folks <br/>  thank you for visiting my website.<br/>
+                <img src="images/icon.png" class="img-responsive"></p>  
+
+                <p class="home-intro medium"><br/> How did it all begin? Just check it out!</p>
+              </div>
+            </div>
+          </div>
+            <div class="row">
+             <div class=" col-sm-12 col-md-4 home-img-box">
+                <figure data-src="super-sy.jpg">
+                    <a href="bio.php" class="cornerLink">
+                        <img src="super-sy.jpg" alt="Super SY" class="img-responsive img-border-round img-responsive">
+                    </a>
+                </figure>             
+              </div>
+               <div class=" col-sm-12 col-md-4 home-img-box">
+                <figure data-src="super-sy.jpg">
+                    <a href="illustrations-portfolio.php" class="cornerLink">
+                        <img class="img-responsive img-border-round img-border" src="wolv-cover-col.jpg"  alt="wolverine">
+                    </a>
+                </figure>             
+              </div>
+               <div class=" col-sm-12 col-md-4 home-img-box">
+                <figure data-src="super-sy.jpg">
+                    <a href="illustrations-portfolio.php" class="cornerLink">
+                        <img class="img-responsive img-border-round img-border" src="Akira-bw0002.jpg"  alt="akira">
+                    </a>
+                </figure>             
+              </div>
+
+           <!--    <div class="col-sm-12 col-md-4 cornerLink">
+                <p class="title-box">See my Porfolio!</p>
+                  <a href="illustrations-portfolio.php" class="cornerLink"><img class="img-responsive img-border-round img-border" src="wolv-cover-col.jpg"  alt="wolverine">
+                </a>
+              </div>
+              <div class="col-sm-12 col-md-4 cornerLink">
+                <p class="title-box">Write me here</p>
+                  <a href="illustrations-portfolio.php" class="cornerLink"><img class="img-responsive img-border-round img-border" src="Akira-bw0002.jpg"  alt="akira">
+                </a>
+              </div> -->
+          
+        <div class="row">
+            <div class="col-sm-12 col-md-12 footer-social">
+                <a href="https://www.facebook.com/peter.payne.399">
+                <i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a href="mailto:peterpayne2002@tiscali.co.uk"    
+                <i class="fa fa-envelope" aria-hidden="true"></i></a>
+           </div>
+        </div>
+        </div>
+      </div> 
+    </div>
+  </body>
+</html>
