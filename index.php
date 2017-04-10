caec2b3f56fbbef445cea94c9a839ad60277b901
Maroua <marua_elba@hotmail.it>
2017-04-10 17:43:59 +0100

    add fontawesome cdn

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="peter/magnific-popup.css">
         <!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
